2d797bf01d341158a3c69d09cebae871056959ed
AshishOnCodes <87165421+lavasurfing@users.noreply.github.com>
2025-06-06 12:20:49 +0530

    Image serve endpoint by id

---
+++ helper.py
@@ -0,0 +1,13 @@
+import os
+
+def get_image_list(folder_path):
+    try:
+        return [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+    except Exception as e:
+        print(f"Error: {e}")
+        return []
+
+
+img_dir = r"C:\Users\Ashish\Documents\Sales Order Scalable - Project\Sales Order-Backend_Service\microservices\components\images"
+files = get_image_list(img_dir)
+print(files[0].split('-')[1].split('.')[0])
--- product_service.py
+++ product_service.py
@@ -8,12 +8,14 @@ import asyncio
 
 # Middleware for CORS
 from fastapi.middleware.cors import CORSMiddleware
-
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 
 
 
 # importing dataset
 from microservices.components.sample_data import product_data
+from helper import get_image_list
 
 
 
@@ -37,10 +39,12 @@ app.add_middleware(
 )
 
 
+
 # Base model data type for product 
 class Product(BaseModel):
     id: int
     qty : int 
+    
     name : str
     price : int
     desc: str
@@ -64,8 +68,24 @@ def get_productById(product_id: int):
         if product['product_id'] == product_id:
             return product
     raise HTTPException(status_code=404, detail="Product not found")
-        
-        
+
+@app.get("/images/{image_id}")
+def get_imageById(image_id: int):
+    image_data = get_image_list("./microservices/components/images")
+    for image in image_data:
+        if image_id == int(image.split('-')[1].split('.')[0]):
+            return FileResponse(f"./microservices/components/images/{image}")
+    raise HTTPException(status_code=404, detail="Image not found")
+    
+
+
+
+    
+ 
+
+
+
+
 if __name__ == "__main__":
     # Run the FastAPI app
     uvicorn.run(app, host="0.0.0.0", port=8000)   
